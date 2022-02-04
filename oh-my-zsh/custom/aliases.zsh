ce480c001ece10e9130bb763beed3264366a9040
Manish Mathai <manishpmathai@gmail.com>
2022-02-03 20:52:31 -0800

    Add alias for mpi for OSX for x86

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -9,4 +9,7 @@ alias brew64='arch -arm64 brew'
 if [ "$(uname)" = "Darwin" ]; then
     # OSX specific aliases
     alias nproc='sysctl -n hw.logicalcpu'
+
+    alias mpicc64='arch -arm64 mpicc'
+    alias mpirun64='arch -arm64 mpirun'
 fi
