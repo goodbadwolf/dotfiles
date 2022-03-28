bc8ffc8a4c446e4626fd06c49dedb961545197c0
Manish Mathai <manishpmathai@gmail.com>
2022-03-27 18:21:20 -0700

    Refactor xreset command

--- oh-my-zsh/custom/beowulf.zsh
+++ oh-my-zsh/custom/beowulf.zsh
@@ -1,11 +1,4 @@
 #!/bin/zsh 
 
-if [ "$(hostname)" = "beowulf"  ]; then
-    xreset() {
-        xrandr \
-            --output HDMI-0 --mode 2560x1440 --pos 1309x0 --rotate normal --size 2560x1440 \
-            --output DP-2 --mode 2560x1440 --pos 2560x1440 --rotate normal \
-            --output DP-4 --primary --mode 2560x1440 --pos 0x1440 --rotate normal
-    }
-    # source ~/apps/fix_displays.sh
+if [ "$(hostname)" = "beowulf"  ]; then   
 fi
