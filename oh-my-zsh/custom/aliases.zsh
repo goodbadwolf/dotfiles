114bae895cfa41181ca512d4e8199de3fda58d31
Manish Mathai <manishpmathai@gmail.com>
2023-09-15 17:55:29 -0700

    Add some more stuff

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -15,3 +15,14 @@ if [ "$(uname)" = "Darwin" ]; then
     alias mpirun64='arch -arm64 mpirun'
     alias nproc='sysctl -n hw.logicalcpu'
 fi
+
+mkdir -p $HOME/.ssh/ctrl
+export SUMMIT_CTRL="$HOME/.ssh/ctrl/mmathai_summit_22"
+alias summit_open='ssh -nNf -o ControlMaster=yes -o ControlPath=$SUMMIT_CTRL summit'
+alias summit_conn='ssh -o ControlPath=$SUMMIT_CTRL summit'
+alias summit_exit='ssh -O exit -o ControlPath=$SUMMIT_CTRL summit'
+
+export FRONTIER_CTRL="$HOME/.ssh/ctrl/mmathai_frontier_22"
+alias frontier_open='ssh -nNf -o ControlMaster=yes -o ControlPath=$FRONTIER_CTRL frontier'
+alias frontier_conn='ssh -o ControlPath=$FRONTIER_CTRL frontier'
+alias frontier_exit='ssh -O exit -o ControlPath=$FRONTIER_CTRL frontier'
