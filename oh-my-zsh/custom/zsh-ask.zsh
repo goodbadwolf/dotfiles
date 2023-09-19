cda1bb7271ce9e0be7488fe9c184d3c26f576add
Manish Mathai <manishpmathai@gmail.com>
2023-09-18 22:01:35 -0700

    Customize zsh-ask

--- oh-my-zsh/custom/zsh-ask.zsh
+++ oh-my-zsh/custom/zsh-ask.zsh
@@ -1,2 +1,5 @@
-export ZSH_ASK_API_KEY="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"
+SCRIPT_PATH=$(dirname ${(%):-%x})
+export ZSH_ASK_API_KEY=$(cat $SCRIPT_PATH/zsh-ask-key.txt)
 export ZSH_ASK_MODEL="gpt-4"
+export ZSH_ASK_TOKENS=8192
+export ZSH_ASK_INHERITS=true
