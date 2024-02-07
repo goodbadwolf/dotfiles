fa9aa3bef61ac4fa9811e1b704d2717df3012ddd
Manish Mathai <manishpmathai@gmail.com>
2024-02-06 20:30:33 -0800

    Changes from Mac

--- oh-my-zsh/custom/aliases_macos.zsh
+++ oh-my-zsh/custom/aliases_macos.zsh
@@ -1,4 +1,4 @@
-if [ "$(uname)" = "Darwin" ]; then
+if [ "$(uname)" = "Darwin"  ]; then
     alias brew64='arch -arm64 brew'
     alias mpicc64='arch -arm64 mpicc'
     alias mpirun64='arch -arm64 mpirun'
